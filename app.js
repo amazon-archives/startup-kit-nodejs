4e8a079099b327ada69a5ba660582d331a891df1
Rabowsky <rabowsky@amazon.com>
2017-02-18 14:48:35 -0800

    CORS enablement

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const express = require('express'),
 
 const app = express();
 app.use(bodyParser.json());
+// CORS enablement
+app.all('/*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+    if ('OPTIONS' == req.method) {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
 
 require('./routes/api')(app, express);
 
