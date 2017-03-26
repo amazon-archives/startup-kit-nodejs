fb51d88ac743fd375be33225cfdff1558345e319
Rabowsky <rabowsky@amazon.com>
2017-03-26 09:01:51 -0700

    collect response times for routes

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 "use strict";
 
-// use nconf to configure variable overriding hierarchy:
+// nconf configures the variable overriding hierarchy:
 // command line -> environment variables -> config file
 const nconf = require('nconf');
 nconf.argv()
     .env()
     .file({ file:'config.json'});
 
-// set up express and routes
+// set up Express and routes
 const express = require('express'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      responseTime = require('response-time');
 
 const app = express();
 app.use(bodyParser.json());
+
+// response time measurement
+app.use(responseTime( (req, res, time) => {
+    let stat = (req.method + req.url)
+                .toUpperCase()
+                .replace(/[:\.]/g, '')
+                .replace(/\//g, '_');
+    console.log('for API call ' + stat + ' response time = ' + time);
+}));
+
 // CORS enablement
 app.all('/*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,7 +39,6 @@ app.all('/*', (req, res, next) => {
 require('./routes/api')(app, express);
 
 // BEGIN LISTENING
-
 app.listen(8081);
 console.log('Listening on port 8081 . . . .');
 
